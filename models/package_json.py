1c03daf15c57b4d0fb72c9b1f1bffb771427e386
ramblehead <v.rybynok@gmail.com>
2021-08-16 21:55:32 +0100

    Updating models/package_json.py from test to a real package.json

--- models/package_json.py
+++ models/package_json.py
@@ -1,6 +1,13 @@
 #!/usr/bin/python3
 # Hey Emacs, this is -*- coding: utf-8; mode: python -*-
 
+from pathlib import Path
+
+from aclib import package_json as aclib_package_json
+
+sParents = Path(__file__).resolve(True).parents
+packageJson = aclib_package_json.loadDominatingPackageJson(sParents)
+
 data = {
-  'test': 'Hey! I am main default test!'
+  'packageJson': packageJson,
 }
